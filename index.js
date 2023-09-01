0acd375605f33caa77f9d95d5c7a507a9e7a7010
Joseph <JosephSped23@gmail.com>
2023-09-01 00:00:28 -0400

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const nameList = ["Gudalupe", "Ollie", "Aki"];
+
+function writeCards(names){
+    let msgs = [];
+    for (let i=0; i < names.length; i++){
+       let currMsg = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        msgs.push(currMsg); 
+    }
+    return msgs;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+
+
+
